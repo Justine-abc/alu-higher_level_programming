969a340d3a4f74bbce680d832e8b60918960302c
root <Justine UMUHOZA>
2024-09-25 10:32:39 -0700

    0-rectangle.js

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-class  Rectangle = require('./0-rectangle');
-
-const r1 = new Rectangle(); 
-console.log(r1);         
-console.log(r1.constructor);
+Rectangle {}
+[class Rectangle]
